9d885944cd9f7eea0db555dc5d11e03f84d31ba7
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-08-13 09:45:39 -0700

    created a playercard container to hold player card data

---
+++ team-builder/src/components/PlayerCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const PlayerCard = () => {
+
+    return (
+        <CardContainer>
+            Hello from Player Card!
+        </CardContainer>
+    )
+}
+
+const CardContainer = styled.div `
+    
+`
