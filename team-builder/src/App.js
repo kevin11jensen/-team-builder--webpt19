b05befa2161ea404ae1a55c0719a6ccd47588c49
Kevin Jensen <kevinjensen1133@yahoo.com>
2020-08-13 07:58:23 -0700

    building out form component

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import Form from './Form'
 import './App.css';
 
 function App() {
+  const [state, setState] = useState([])
   return (
     <div className="App">
-      hello from app
+      <Form />
     </div>
   );
 }
---
+++ team-builder/src/Form.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+export default function Form() {
+    
+    return (
+        <form>
+            <input
+            placeholder = 'player name'
+            />
+            <input
+            placeholder = 'player number'
+            />
+            <input
+            placeholder = 'player position'
+            />
+            <input
+            placeholder = 'player team'
+            />
+        </form>
+    );
+}
+
+
